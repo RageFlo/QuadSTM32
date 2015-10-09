494e2e6c3132a321573cd2683e24281e1a1ec516
RageFlo <spamfach007@online.de>
2015-10-09 10:35:19 +0200

    Tidy stdout
    tidy up

--- stdout_USART.c
+++ stdout_USART.c
@@ -60,74 +60,6 @@ static uint8_t input_buf[BUFFERSIZE_IN];
 static uint8_t command[BUFFERSIZE_COM];
 
  
-/**
-  Initialize stdout
- 
-  \return          0 on success, or -1 on error.
-*/
-int std_init (void) {
-  int32_t status;
- 
-  status = ptrUSART->Initialize(myUSART_callback);
-  if (status != ARM_DRIVER_OK) return (-1);
- 
-  status = ptrUSART->PowerControl(ARM_POWER_FULL);
-  if (status != ARM_DRIVER_OK) return (-1);
- 
-  status = ptrUSART->Control(ARM_USART_MODE_ASYNCHRONOUS |
-                             ARM_USART_DATA_BITS_8       |
-                             ARM_USART_PARITY_NONE       |
-                             ARM_USART_STOP_BITS_1       |
-                             ARM_USART_FLOW_CONTROL_NONE,
-                             USART_BAUDRATE);
-  if (status != ARM_DRIVER_OK) return (-1);
-
-  status = ptrUSART->Control(ARM_USART_CONTROL_TX , 1);
-	status = ptrUSART->Control(ARM_USART_CONTROL_RX , 1);
-  if (status != ARM_DRIVER_OK) return (-1);
-
-  return (0);
-}
- 
- 
-/**
-  Put a character to the stdout
- 
-  \param[in]   ch  Character to output
-  \return          The character written, or -1 on write error.
-*/
-int stdout_putchar (int ch) {
-  uint8_t buf[1];
- 
-  buf[0] = ch;
-  if (ptrUSART->Send(buf, 1) != ARM_DRIVER_OK) {
-    return (-1);
-  }
-  while (ptrUSART->GetTxCount() != 1);
-  return (ch);
-}
-
-/**
-  Get a character from stdin
- 
-  \return     The next character from the input, or -1 on read error.
-*/
-int stdin_getchar (void) {
-  uint8_t buf[1];
- 
-  if (ptrUSART->Receive(buf, 1) != ARM_DRIVER_OK) {
-    return (-1);
-  }
-  while (ptrUSART->GetRxCount() != 1);
-  return (buf[0]);
-}
-
-int startRec(uint8_t* buffer){
-	if (ptrUSART->Receive(buffer, BUFFERSIZE_IN) != ARM_DRIVER_OK) {
-    return (-1);
-  }
-	return 0;
-}
 
 
 
@@ -196,6 +128,74 @@ void kommuHandler(void){
 	if(buildCommand(input_buf,command)){
 		puts((char*)command);
 	}
+}
+
+int startRec(uint8_t* buffer){
+	if (ptrUSART->Receive(buffer, BUFFERSIZE_IN) != ARM_DRIVER_OK) {
+    return (-1);
+  }
+	return 0;
+}
+
+/**
+  Initialize stdout
+ 
+  \return          0 on success, or -1 on error.
+*/
+int std_init (void) {
+  int32_t status;
+ 
+  status = ptrUSART->Initialize(myUSART_callback);
+  if (status != ARM_DRIVER_OK) return (-1);
+ 
+  status = ptrUSART->PowerControl(ARM_POWER_FULL);
+  if (status != ARM_DRIVER_OK) return (-1);
+ 
+  status = ptrUSART->Control(ARM_USART_MODE_ASYNCHRONOUS |
+                             ARM_USART_DATA_BITS_8       |
+                             ARM_USART_PARITY_NONE       |
+                             ARM_USART_STOP_BITS_1       |
+                             ARM_USART_FLOW_CONTROL_NONE,
+                             USART_BAUDRATE);
+  if (status != ARM_DRIVER_OK) return (-1);
+
+  status = ptrUSART->Control(ARM_USART_CONTROL_TX , 1);
+	status = ptrUSART->Control(ARM_USART_CONTROL_RX , 1);
+  if (status != ARM_DRIVER_OK) return (-1);
+
+  return (0);
+}
+ 
+/**
+  Put a character to the stdout
+ 
+  \param[in]   ch  Character to output
+  \return          The character written, or -1 on write error.
+*/
+int stdout_putchar (int ch) {
+  uint8_t buf[1];
+ 
+  buf[0] = ch;
+  if (ptrUSART->Send(buf, 1) != ARM_DRIVER_OK) {
+    return (-1);
+  }
+  while (ptrUSART->GetTxCount() != 1);
+  return (ch);
+}
+
+/**
+  Get a character from stdin
+ 
+  \return     The next character from the input, or -1 on read error.
+*/
+int stdin_getchar (void) {
+  uint8_t buf[1];
+ 
+  if (ptrUSART->Receive(buf, 1) != ARM_DRIVER_OK) {
+    return (-1);
+  }
+  while (ptrUSART->GetRxCount() != 1);
+  return (buf[0]);
 }
 
 int stderr_putchar (int ch){
